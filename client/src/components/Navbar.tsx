9c58c50e8a4ec9b8852ccbd1f7eddb15c79cba0d
EstebanLuu <estebaanlunaaa@gmail.com>
2022-10-25 11:54:06 -0300

    navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,11 +3,32 @@ import styled from "styled-components";
 import { Themes } from "./Theme/Theme";
 
 const Navbar = () => {
-  return <NavbarContainer>Navbar</NavbarContainer>;
+  return (
+    <NavbarContainer>
+      <div className="content">
+        <span className="nav__logo">Tu Negocio.</span>
+        <div className="nav__searchbar"></div>
+        <div className="nav__user"></div>
+      </div>
+    </NavbarContainer>
+  );
 };
 
 export default Navbar;
 
 const NavbarContainer = styled.div`
-  color: ${Themes.primary};
+  width: 100%;
+
+  .content {
+    font-family: Poppins;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    background-color: ${Themes.primary};
+    height: 50px;
+  }
+
+  .nav__logo {
+    color: ${Themes.secondary};
+  }
 `;

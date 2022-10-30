3c65fab8e284057e6ba322f862cafff8af712f4e
andresrh11 <andresr10020@gmail.com>
2022-10-30 18:58:49 -0500

    responsive forms

--- client/src/components/Recover/FormRecover.tsx
+++ client/src/components/Recover/FormRecover.tsx
@@ -5,37 +5,58 @@ export function FormRecover(){
 
         <>
             <RecoverContainer>
+                <form  autoComplete="off">
                 <div>
                     <Logo>PedirYa</Logo>
                 </div>
                 Olvidó su contraseña?
                 <input className="input__recover" placeholder="Email" name='recover' />
                 <button type="submit" className="boton">Recuperar</button>
+                </form>
             </RecoverContainer>
         
         </>
     )
 }
 const RecoverContainer = styled.div`
-position:relative;
-width: 450px;
-height: 600px;
-align-items:center;
-align-content:center;
-transition: all .2s ease;
-box-shadow: 3px 3px 20px ${({ theme }) => theme.secondary};
-border-radius: 15px;
-display:flex;
-flex-flow:column;
-border: 3px ${({ theme }) => theme.secondary} solid;
-justify-content: space-evenly;
+ width: 100%;
+    height: 100vh;
+    display:flex;
+    justify-content: center;
+    align-items:center;
+    form{ position:relative;
+    width: 450px;
+    height: 600px;
+    align-items:center;
+    
+    transition: all .2s ease;
+    box-shadow: 3px 3px 20px ${({ theme }) => theme.secondary};
+    border-radius: 15px;
+    display:flex;
+    flex-flow:column;
+    border: 3px ${({ theme }) => theme.secondary} solid;
+    justify-content: space-evenly;
+    font-size:17px;
+}
+
+    @media screen and (max-width: 500px) {
+    width: 100%;
+    form{ width:100%;
+    border:none;
+    box-shadow:none;
+    margin:0;
+    padding-top:100px;
+    }
+}
 
 
 font-size:17px;
 .input__recover{
-    border:none;
-    font-size: 18px;
-    margin-left:5px
+    border: 1px solid ${({ theme }) => theme.border};
+  font-size: 18px;
+  padding: 7px;
+  border-radius: 7px;
+  margin-left:7px
 }
 .boton{
     font-size: 18px;

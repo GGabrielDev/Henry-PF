cbfd3f96fe57d3d7e3c2a9c78b3e93f7566da8fa
EstebanLuu <estebaanlunaaa@gmail.com>
2022-10-30 22:11:09 -0300

    detalle

--- client/src/components/Cards/Cards.tsx
+++ client/src/components/Cards/Cards.tsx
@@ -1,25 +1,33 @@
 import { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "../../app/hooks"
-import { getProducts, selectProducts } from "../../features/products/productSlice"
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import {
+  getProducts,
+  selectProducts,
+} from "../../features/products/productSlice";
 import Card from "../Card";
 
-export default function Cards(){
-    const products = useAppSelector(selectProducts);
-    const dispatch= useAppDispatch();
-    console.log(products)
-    useEffect(()=>{
-        dispatch(getProducts())
-    },[])
+export default function Cards() {
+  const products = useAppSelector(selectProducts);
+  const dispatch = useAppDispatch();
+  console.log(products);
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
 
-    return(
+  return (
     <>
-        <div>
-            {products.map((e)=>{
-                return(
-                    <Card key={e.id} name={e.name} price_local={e.price_local} image={e.image} />
-                )
-            })}
-        </div>
+      <div>
+        {products?.map((e) => {
+          return (
+            <Card
+              key={e.id}
+              name={e.name}
+              price_local={e.price_local}
+              image={e.image}
+            />
+          );
+        })}
+      </div>
     </>
-    )
+  );
 }

7f43d584da6ab62085fe7874952a671521ff81b6
andresrh11 <andresr10020@gmail.com>
2022-10-30 20:01:58 -0500

    para jose

--- client/src/components/Cards/Cards.tsx
+++ client/src/components/Cards/Cards.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import { getProducts, selectProducts } from "../../features/products/productSlice"
 import Card from "../Card";
-
+import styled from "styled-components";
 export default function Cards(){
     const products = useAppSelector(selectProducts);
     const dispatch= useAppDispatch();
@@ -13,13 +13,22 @@ export default function Cards(){
 
     return(
     <>
-        <div>
-            {products.map((e)=>{
+        <DivCards>
+            {products?.map((e)=>{
                 return(
+
                     <Card key={e.id} name={e.name} price_local={e.price_local} image={e.image} />
                 )
             })}
-        </div>
+        </DivCards>
     </>
     )
 }
+const DivCards = styled.div`
+    background-color: blue;
+    width:100%;
+    display:flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+   
+`

5f7123c2edd4f7798236179dca2a8f8855dce407
Jose Soria <josesoriap21@gmail.com>
2022-11-01 12:53:51 -0300

    landingpage

---
+++ client/src/components/Navbarlanding.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from "styled-components";
+
+
+const Navbarlanding = () => {
+  return (
+    <Navbarlandings>Navbarlanding</Navbarlandings>
+  )
+}
+
+const Navbarlandings = styled.div`
+width: 100%;
+background-color: blue;
+position: absolute;
+    
+`
+
+export default Navbarlanding

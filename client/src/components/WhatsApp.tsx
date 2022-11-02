7f0ef2eaa0a30d786591b598d76a0ee706a8bcd2
andresrh11 <andresr10020@gmail.com>
2022-11-02 08:47:42 -0500

    mensaje wapp

--- client/src/components/WhatsApp.tsx
+++ client/src/components/WhatsApp.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 import Whatsapp from "../assets/imagenesSlider/iconoWTP.png";
+const user=[{
+  nombre:'carlos',
+  apellido:'lopez',
+  
+}]
 const carrito = [
   {
     Nombre: "producto 1",
@@ -14,16 +19,19 @@ const carrito = [
     Monto: 200.0,
   },
 ];
+const numero='+573053721294'
 const WhatsApp = () => {
+  
   let productosParaWsp = carrito.map(
     (producto) => `- ${producto.Nombre}, $${producto.Monto}`
   );
+  let datosCliente = user.map((e:any) => `-Nombre: ${e.nombre} \n -Apellido: ${e.apellido}`)
   const productosConFormatoAmigable = productosParaWsp.join("\n");
   return (
     <a
         href={
-          "https://api.whatsapp.com/send?phone=+573053721294&text=Me%20interesan%20los%20siguientes%20productos:" +
-          " " +
+          `https://api.whatsapp.com/send?phone=${numero}&text=Hola%E2%98%BA%0D%0A%F0%9F%93%82Mis+datos+son%3A%0D%0A${datosCliente}%0D%0AEstoy+interesado+en+los+siguientes+productos+de+la+pagina%F0%9F%94%A5%3A` +
+          " " + 
           productosConFormatoAmigable
         }
       >

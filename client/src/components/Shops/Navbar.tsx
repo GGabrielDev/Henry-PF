d595eae2292614092620585e51a8a8ed31c67c63
GGabrielDev <luisgabrielramosrobles@gmail.com>
2022-11-16 10:44:07 -0400

    Extracted Navbar and ShoppingCar

---
+++ client/src/components/Shops/Navbar.tsx
@@ -0,0 +1,314 @@
+import styled from "styled-components";
+import { BiUser, BiShoppingBag } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import {
+  actions as cartActions,
+  selectors as cartSelectors,
+} from "../../features/cart/cartSlice";
+import {
+  selectors as sellerSelectors,
+  SellerType,
+} from "../../features/sellers/sellerSlice";
+import ShoppingCart from "./ShoppingCart";
+import { selectProducts } from "../../features/products/productSlice";
+
+const { toggleCart } = cartActions;
+const { selectCartQuantity, selectIsOpen } = cartSelectors;
+const { selectSeller } = sellerSelectors;
+
+const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const seller = useAppSelector(selectSeller) as NonNullable<SellerType>;
+  const cartQuantity = useAppSelector(selectCartQuantity);
+  const isOpen = useAppSelector(selectIsOpen);
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+  const categorias = useAppSelector(selectProducts);
+
+  return (
+    <NavbarContainer>
+      <ShoppingCart isOpen={isOpen} />
+      <Header>
+        <Headertop>
+          <Infotop>
+            <div className="infotop-item">
+              {isAuthenticated ? (
+                <div
+                  className="is"
+                  onClick={() =>
+                    logout({
+                      returnTo:
+                        window.location.origin + window.location.pathname,
+                    })
+                  }
+                >
+                  {" "}
+                  Salir de sesión{" "}
+                </div>
+              ) : (
+                <div
+                  className="is"
+                  onClick={() =>
+                    loginWithRedirect({
+                      returnTo:
+                        window.location.origin + window.location.pathname,
+                    })
+                  }
+                >
+                  {" "}
+                  Iniciar sesion{" "}
+                </div>
+              )}
+            </div>
+            <div className="infotop-item">
+              {" "}
+              <Link to="/usuario" className="infotop-item-link">
+                Perfil
+              </Link>{" "}
+            </div>
+            <div className="infotop-item">
+              <Link className="infotop-item-link" to="/">
+                {" "}
+                Henry Shops
+              </Link>
+            </div>
+          </Infotop>
+        </Headertop>
+        <Headermiddle>
+          <Link to={`/shop/${seller.nombreUrl}`}>
+            <Logo>{seller.nombreNegocio}</Logo>
+          </Link>
+
+          <Userinfo>
+            <div className="userinfo-item userinfo-text">Notebooks</div>
+            <div className="userinfo-item userinfo-text">Cpu</div>
+            <div className="userinfo-item">
+              {/* <Link to="/usuario/general">
+                <BiUser className="icono__user" />
+              </Link> */}
+            </div>
+
+            <div className="userinfo-item">
+              <button
+                className="button__cart"
+                onClick={() => dispatch(toggleCart())}
+              >
+                <BiShoppingBag /> {cartQuantity}
+              </button>
+            </div>
+          </Userinfo>
+        </Headermiddle>
+        <Headerbottom>
+          <Infocategories>
+            {categorias
+              ? categorias.map((e) => {
+                  return <div>{e.categories}</div>;
+                })
+              : null}
+          </Infocategories>
+        </Headerbottom>
+      </Header>
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
+
+const NavbarContainer = styled.div`
+  width: 100%;
+
+  position: relative;
+
+  .userinfo-item {
+    cursor: pointer;
+  }
+
+  .userinfo-item button {
+    svg {
+      color: ${({ theme }) => theme.light};
+    }
+  }
+
+  .userinfo-text {
+    font-weight: 400;
+    color: ${({ theme }) => theme.details};
+    transition: 0.5s;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  .infocategories-item {
+    font-weight: 500;
+    font-size: 14px;
+    cursor: pointer;
+    transition: 0.5s;
+    &:hover {
+      color: ${({ theme }) => theme.primary};
+    }
+  }
+
+  .infotop-item {
+    font-size: 12px;
+    span {
+      cursor: poniter;
+    }
+  }
+
+  .infotop-item-link {
+    color: ${({ theme }) => theme.dark};
+    text-decoration: none;
+  }
+
+  @media screen and (max-width: 1000px) {
+    .userinfo-text {
+      display: none;
+    }
+  }
+`;
+
+const Header = styled.div`
+  width: 100%;
+  position: absolute;
+  z-index: 10;
+`;
+
+const Headertop = styled.div`
+  height: 30px;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  border-bottom: 1.3px solid #f0f0f0;
+
+  @media screen and (max-width: 500px) {
+    justify-content: center;
+  }
+`;
+
+const Infotop = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 310px;
+  margin-right: 50px;
+
+  .infotop-item {
+    font-size: 12px;
+  }
+
+  .button__cart {
+    position: relative;
+  }
+
+  .infotop-item-ini {
+    color: ${({ theme }) => theme.dark};
+  }
+  .is {
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: 500px) {
+    margin: 0;
+  }
+`;
+
+const Headermiddle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 100px;
+  border-bottom: 1.3px solid #f0f0f0;
+
+  @media screen and (max-width: 1000px) {
+    padding: 0 20px;
+  }
+`;
+
+export const Logo = styled.div`
+  width: 200px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 40px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.primary};
+
+  @media screen and (max-width: 1000px) {
+    width: auto;
+    justify-content: start;
+    font-size: 30px;
+  }
+`;
+
+const Userinfo = styled.div`
+  width: 400px;
+  height: 60px;
+  display: flex;
+  font-weight: 600;
+  align-items: center;
+  justify-content: space-between;
+
+  button {
+    border-radius: 7px;
+    background-color: ${({ theme }) => theme.primary};
+    color: ${({ theme }) => theme.light};
+    border: 1px solid transparent;
+    padding: 7px 10px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    svg {
+      font-size: 20px;
+      margin-right: 5px;
+    }
+    &:hover {
+      border: 1px solid ${({ theme }) => theme.primary};
+      background-color: ${({ theme }) => theme.light};
+      color: ${({ theme }) => theme.primary};
+      svg {
+        color: ${({ theme }) => theme.primary};
+      }
+    }
+  }
+
+  svg {
+    font-size: 30px;
+  }
+  svg {
+    font-size: 30px;
+    text-decoration: none;
+    color: ${({ theme }) => theme.dark};
+  }
+
+  @media screen and (max-width: 1000px) {
+    width: 150px;
+  }
+  @media screen and (max-width: 350px) {
+    width: 130px;
+  }
+`;
+
+const Headerbottom = styled.div`
+  display: flex;
+  align-items: center;
+  height: 40px;
+
+  @media screen and (max-width: 1000px) {
+    justify-content: center;
+  }
+
+  @media screen and (max-width: 500px) {
+    display: none;
+  }
+`;
+
+const Infocategories = styled.div`
+  margin-left: 100px;
+  display: flex;
+  width: 400px;
+  justify-content: space-between;
+
+  @media screen and (max-width: 1000px) {
+    margin: 0;
+  }
+`;
---
+++ client/src/components/Shops/ShoppingCart/index.tsx
@@ -0,0 +1,176 @@
+import { SyntheticEvent } from "react";
+import styled from "styled-components";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { useAppDispatch, useAppSelector } from "../../../app/hooks";
+import { selectProducts } from "../../../features/products/productSlice";
+import { actions, selectors } from "../../../features/cart/cartSlice";
+import { CartItem } from "./CartItem";
+import { useAuth0 } from "@auth0/auth0-react";
+
+type ShoppingCartProps = {
+  isOpen: boolean;
+};
+
+const { toggleCart } = actions;
+const { selectCartItems } = selectors;
+
+export default function SoppingCart({ isOpen }: ShoppingCartProps) {
+  const dispatch = useAppDispatch();
+  const cartItems = useAppSelector(selectCartItems);
+  const item = useAppSelector(selectProducts);
+
+  const handleBuy = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    alert("Coming soon");
+  };
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
+
+  return (
+    <>
+      {isOpen ? (
+        <ShoppingCart>
+          <ShoppingCartContainer>
+            <div className="menu">
+              <div className="content" onClick={() => dispatch(toggleCart())}>
+                <span className="close">
+                  <AiOutlineArrowLeft />
+                </span>
+                <h1 className="title__cart">Cart</h1>
+              </div>
+              <div className="info__carro">
+                {cartItems.map((item: any) => (
+                  <div className="info__carta" key={item.product.id}>
+                    <CartItem {...item} />
+                  </div>
+                ))}
+              </div>
+              <div className="info__total">
+                <div className="total__price">
+                  Total:
+                  <div className="total">
+                    {cartItems.reduce((total: any, cartItem: any) => {
+                      const itemFind = item.find(
+                        (e) => e.id === cartItem.product.id
+                      );
+                      return (
+                        total + (itemFind?.price_local || 0) * cartItem.quantity
+                      );
+                    }, 0)}
+                  </div>
+                </div>
+                {isAuthenticated ? (
+                  <button className="comprar__cart" onClick={handleBuy}>
+                    Comprar
+                  </button>
+                ) : (
+                  <button
+                    className="comprar__cart"
+                    onClick={() => loginWithRedirect()}
+                  >
+                    Loguea y compra!
+                  </button>
+                )}
+              </div>
+            </div>
+          </ShoppingCartContainer>
+        </ShoppingCart>
+      ) : (
+        ""
+      )}
+    </>
+  );
+}
+
+const ShoppingCart = styled.div`
+  display: flex;
+  z-index: 1000;
+`;
+
+const ShoppingCartContainer = styled.div`
+  z-index: 1000;
+
+  .menu {
+    position: absolute;
+    width: 400px;
+    min-height: 100px;
+    background-color: #ffffff;
+    right: 0px;
+    top: 0;
+    border: 1px solid ${({ theme }) => theme.border};
+    transition: 0.6s all ease-in-out;
+    border-radius: 0 0 10px 10px;
+  }
+
+  .content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    transition: 0.4s;
+    padding: 5px;
+    &:hover {
+      color: ${({ theme }) => theme.primary};
+    }
+  }
+
+  .close {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 20px;
+    margin-right: 5px;
+  }
+
+  .title__cart {
+    text-align: center;
+    font-size: 20px;
+  }
+
+  .info__carro {
+    /* display: flex; */
+    width: 100%;
+    flex-direction: column;
+  }
+
+  .info__carta {
+    width: 95%;
+  }
+
+  .info__total {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin-bottom: 10px;
+    color: ${({ theme }) => theme.primary};
+  }
+
+  .total {
+    margin-left: 5px;
+  }
+
+  .total__price {
+    display: flex;
+  }
+
+  .comprar__cart {
+    padding: 5px;
+    border: 1px solid transparent;
+    transition: 0.4s;
+    background-color: ${({ theme }) => theme.primary};
+    color: ${({ theme }) => theme.light};
+    border-radius: 5px;
+    cursor: pointer;
+    &:hover {
+      border: 1px solid ${({ theme }) => theme.primary};
+      color: ${({ theme }) => theme.primary};
+      background-color: ${({ theme }) => theme.light};
+    }
+  }
+
+  @media screen and (max-width: 500px) {
+    .menu {
+      width: 100%;
+    }
+  }
+`;

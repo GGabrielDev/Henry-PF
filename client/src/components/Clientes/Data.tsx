4536fabaa38fc93e1f615b60297a268ff2a160e1
GGabrielDev <luisgabrielramosrobles@gmail.com>
2022-11-17 12:40:55 -0400

    Troubleshooting Deploy #7

--- client/src/components/Clientes/Data.tsx
+++ client/src/components/Clientes/Data.tsx
@@ -5,23 +5,20 @@ import Work4 from "../../assets/ImagenesClients/Perfumeria.jpg";
 import Work5 from "../../assets/imagenesSlider/hamburguesas.jpg";
 import Work6 from "../../assets/imagenesSlider/5fa8fb32d99eb.jpeg";
 
-
-
 export const projectsData = [
-
   {
     id: 1,
     image: Work1,
     Title: "Tu Gamer",
     Category: "Entretenimiento",
-    Demo: "/shop/tugamer",
+    Demo: "/tugamer",
   },
   {
     id: 2,
     image: Work2,
     Title: "Tu Pancito",
     Category: "Gastronomia",
-    Demo: "/shop/tupancito",
+    Demo: "/tugamer",
   },
   {
     id: 3,
@@ -42,7 +39,7 @@ export const projectsData = [
     image: Work5,
     Title: "Tu Hamburguesa",
     Category: "Gastronomia",
-    Demo: "/shop/tuhamburguesa",
+    Demo: "/tugamer",
   },
   {
     id: 6,
